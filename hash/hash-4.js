9e4a7c28430eecd5f20590f34c22a62a8571a9ac
powdercat <powdercatone@gmail.com>
2023-08-12 23:46:50 +0900

    add: hash-4 풀이

---
+++ hash/hash-4.js
@@ -0,0 +1,30 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42578
+ */
+
+function solution(clothes) {
+  var answer = 0;
+  const obj = {};
+  clothes.map(cloth => {
+    if (obj[cloth[1]]) {
+      obj[cloth[1]] += 1;
+    } else {
+      obj[cloth[1]] = 1;
+    }
+  });
+
+  return Object.values(obj).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
+}
+
+/**
+ * example
+ * A 2개 (ㄱ, ㄴ)
+ * B 2개 (ㄷ, ㄹ)
+ * C 1개 (ㅁ, ㅂ)
+ * (2 + 1) * (2 + 1) * (1 + 1) - 1
+ *
+ * A 선택할 수 있는 경우의 수 = 3개 (ㄱ, ㄴ, x)
+ * B 선택할 수 있는 경우의 수 = 3개 (ㄷ, ㄹ, x)
+ * C 선택할 수 있는 경우의 수 = 2개 (ㅁ, ㅂ, x)
+ * 모두 x 가 되는 경우의 수 = 1개 제외
+ */
